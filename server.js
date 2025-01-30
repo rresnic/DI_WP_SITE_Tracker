b107b5a6b2e6b99ee697bd45f72c1dff4c096931
Reuven Resnicoff <yresnicoff@gmail.com>
2025-01-30 15:49:02 +0200

    initial creation of express server and testing connection to the database.

---
+++ server.js
@@ -0,0 +1,26 @@
+const {db} = require("./config/db.js");
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+const app = express();
+const {PORT} = process.env;
+app.listen(PORT || 5002, ()=>{
+    console.log(`running on ${PORT || 5002}`);
+})
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    credentials: true,
+    // origin: ['http://localhost:5173']
+}))
+async function test(){
+    try {
+        const response = await db.raw('select version()');
+        console.log(response.rows);
+    } catch (error) {
+        console.log(error);
+    }
+}
+// test();
